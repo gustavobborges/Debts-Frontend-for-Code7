da77de297f0f3b1d5e4d42ccb03e90ca2234751c
Gustavo Borges <gustavoborges98@hotmail.com>
2021-07-03 19:45:31 -0300

    txt of time project

---
+++ src/pages/Debts.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './Debts.css';
+
+export default function PagesDebts() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
