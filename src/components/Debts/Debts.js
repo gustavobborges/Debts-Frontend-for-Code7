cf4adb51852ffe3f6ab8d791c123d451c1c5fc2c
Gustavo Borges <gustavoborges98@hotmail.com>
2021-07-05 21:26:25 -0300

    delete de divida e correcao de inconsistencias

--- src/components/Debts/Debts.js
+++ src/components/Debts/Debts.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { BsSearch } from 'react-icons/bs';
+import { BsSearch, BsFillTrashFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
-
 import DataTable from 'react-data-table-component';
 import './Debts.css';
-
+import axios from 'axios';
 import api_debts from '../../services/apiDebts';
 import api_clients from '../../services/apiDebts';
 
 export default function Debts() {
-
 	const [debts, setDebts] = useState([]);
 	const [clients, setClients] = useState([]);
 
@@ -17,7 +15,7 @@ export default function Debts() {
 		api_debts.get()
 			.then(response => {
 				console.log(response.data);
-				setDebts(response.data);
+				setDebts(response.data.result);
 			})
 			.catch((error) => {
 				console.log("Problema durante a consulta por dívidas." + error);
@@ -25,30 +23,42 @@ export default function Debts() {
 	}, []);
 
 	useEffect(() => {
-        api_clients.get().then(response => setClients(response.data));
-    }, [clients]);
+		api_clients.get().then(response => setClients(response.data));
+	}, [clients]);
+
+	async function DeleteDebt(id) {
+		await axios.delete(`https://provadev.xlab.digital/api/v1/divida/${id}?uuid=317a7ff8-7454-4131-8cc7-8ed2ebc141bd`)
+			.then((response) => {
+				const currentDebts = debts.filter(debt => debt._id !== id);
+				setDebts(currentDebts);
+			})
+			.catch((error) => {
+				console.log("Problema durante a exclusão por dívidas." + error);
+			})
+	}
 
 	const columns = [
 		{
 			name: "Cliente",
-			selector: "idUsuario",
+			selector: (row) => row['idUsuario'],
 			sortable: true,
 		},
 		{
 			name: "Motivo",
-			selector: "motivo",
+			selector: (row) => row['motivo'],
 			sortable: true,
 		},
 		{
 			name: "Valor",
-			selector: "valor",
+			selector: (row) => row['valor'],
 			sortable: true,
 		},
 		{
 			name: "Ações",
 			cell: (row) => [
 				<div className="col_action" key={row._id} data={row}>
-					<Link to={`/edit/${row._id}`}><BsSearch /></Link>
+					<Link to={`/edit/${row._id}`} className="button_table button_edit"><BsSearch /></Link>
+					<Link to="/" onClick={() => DeleteDebt(row._id)} className="button_table button_delete"><BsFillTrashFill /></Link>
 				</div>
 			],
 			ignoreRowClick: true,
@@ -66,14 +76,14 @@ export default function Debts() {
 				<DataTable
 					title="Tabela de Débitos"
 					columns={columns}
-					data={debts.result}
+					data={debts}
 					defaultSortField="_id"
 					pagination
 				>
 				</DataTable>
 			</div>
 			<div className="debts_foot">
-				<Link to="/create" className="button button_create">Adicionar</Link>
+				<Link to="/create" className="button_table button_create">Adicionar</Link>
 			</div>
 		</div>
 	)
